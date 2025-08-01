6aec6d36a4ce0a1368eede3a0cb3c7c0c74fc8eb
kashif12 <kashif12@gmail.com>
2025-08-01 18:36:04 +0530

    Work done in client on test 06

--- src/pages/projects_component/ProjectHeader.js
+++ src/pages/projects_component/ProjectHeader.js
@@ -10,81 +10,81 @@ import EnquiryForm from '../../component/EnquiryForm';
 
 const ProjectHeader = ({ project }) => {
     const bhkTypes = [
-    {
-        key: `${project.Beds_bhk} BHK ${Additional_Room}`,
-        beds: project.Beds_bhk,
-        superArea: project.Super_Area_bhk,
-        builtupArea: project.Built_Up_Area_bhk,
-        carpetArea: project.Carpet_Area_bhk,
-        img: project.Floor_Plan_bhk,
-    },
-    {
-        key: `${project.Beds_bhk1} BHK ${Additional_Room1}`,
-        beds: project.Beds_bhk1,
-        superArea: project.Super_Area_bhk1,
-        builtupArea: project.Built_Up_Area_bhk1,
-        carpetArea: project.Carpet_Area_bhk1,
-        img: project.Floor_Plan_bhk1,
-    },
-    {
-        key: `${project.Beds_bhk2} BHK ${Additional_Room2}`,
-        beds: project.Beds_bhk2,
-        superArea: project.Super_Area_bhk2,
-        builtupArea: project.Built_Up_Area_bhk2,
-        carpetArea: project.Carpet_Area_bhk2,
-        img: project.Floor_Plan_bhk2,
-    },
-    {
-        key: `${project.Beds_bhk3} BHK ${Additional_Room3}`,
-        beds: project.Beds_bhk3,
-        superArea: project.Super_Area_bhk3,
-        builtupArea: project.Built_Up_Area_bhk3,
-        carpetArea: project.Carpet_Area_bhk3,
-        img: project.Floor_Plan_bhk3,
-    },
-    {
-        key: `${project.Beds_bhk4} BHK ${Additional_Room4}`,
-        beds: project.Beds_bhk4,
-        superArea: project.Super_Area_bhk4,
-        builtupArea: project.Built_Up_Area_bhk4,
-        carpetArea: project.Carpet_Area_bhk4,
-        img: project.Floor_Plan_bhk4,
-    },
-    {
-        key: `${project.Beds_bhk5} BHK ${Additional_Room5}`,
-        beds: project.Beds_bhk5,
-        superArea: project.Super_Area_bhk5,
-        builtupArea: project.Built_Up_Area_bhk5,
-        carpetArea: project.Carpet_Area_bhk5,
-        img: project.Floor_Plan_bhk5,
-    },
-    {
-        key: "Penthouse",
-        beds: project.Penthouse,
-        superArea: project.Super_Area_Penthouse,
-        builtupArea: project.Built_Up_Area_Penthouse,
-        carpetArea: project.Carpet_Area_Penthouse,
-        img: project.Floor_Plan_Penthouse,
-    }
-];
+        {
+            key: `${project.Beds_bhk?.toString().trim().slice(0, 1)} BHK ${project.Additional_Room ? ' + ' + project.Additional_Room.trim() : ''}`,
+            beds: project.Beds_bhk,
+            superArea: project.Super_Area_bhk,
+            builtupArea: project.Built_Up_Area_bhk,
+            carpetArea: project.Carpet_Area_bhk,
+            img: project.Floor_Plan_bhk,
+        },
+        {
+            key: `${project.Beds_bhk1?.toString().trim().slice(0, 1)} BHK${project.Additional_Room1 ? ' + ' + project.Additional_Room1.trim() : ''}`,
+            beds: project.Beds_bhk1,
+            superArea: project.Super_Area_bhk1,
+            builtupArea: project.Built_Up_Area_bhk1,
+            carpetArea: project.Carpet_Area_bhk1,
+            img: project.Floor_Plan_bhk1,
+        },
+        {
+            key: `${project.Beds_bhk2?.toString().trim().slice(0, 1)} BHK${project.Additional_Room2 ? ' + ' + project.Additional_Room2.trim() : ''}`,
+            beds: project.Beds_bhk2,
+            superArea: project.Super_Area_bhk2,
+            builtupArea: project.Built_Up_Area_bhk2,
+            carpetArea: project.Carpet_Area_bhk2,
+            img: project.Floor_Plan_bhk2,
+        },
+        {
+            key: `${project.Beds_bhk3?.toString().trim().slice(0, 1)} BHK${project.Additional_Room3 ? ' + ' + project.Additional_Room3.trim() : ''}`,
+            beds: project.Beds_bhk3,
+            superArea: project.Super_Area_bhk3,
+            builtupArea: project.Built_Up_Area_bhk3,
+            carpetArea: project.Carpet_Area_bhk3,
+            img: project.Floor_Plan_bhk3,
+        },
+        {
+            key: `${project.Beds_bhk4?.toString().trim().slice(0, 1)} BHK${project.Additional_Room4 ? ' + ' + project.Additional_Room4.trim() : ''}`,
+            beds: project.Beds_bhk4,
+            superArea: project.Super_Area_bhk4,
+            builtupArea: project.Built_Up_Area_bhk4,
+            carpetArea: project.Carpet_Area_bhk4,
+            img: project.Floor_Plan_bhk4,
+        },
+        {
+            key: `${project.Beds_bhk5?.toString().trim().slice(0, 1)} BHK${project.Additional_Room5 ? ' + ' + project.Additional_Room5.trim() : ''}`,
+            beds: project.Beds_bhk5,
+            superArea: project.Super_Area_bhk5,
+            builtupArea: project.Built_Up_Area_bhk5,
+            carpetArea: project.Carpet_Area_bhk5,
+            img: project.Floor_Plan_bhk5,
+        },
+        {
+            key: "Penthouse",
+            beds: project.Penthouse,
+            superArea: project.Super_Area_Penthouse,
+            builtupArea: project.Built_Up_Area_Penthouse,
+            carpetArea: project.Carpet_Area_Penthouse,
+            img: project.Floor_Plan_Penthouse,
+        }
+    ];
 
-const configurationsData = Object.fromEntries(
-    bhkTypes
-        .filter(item =>
-            item.key === "Penthouse"
-                ? !!item.beds // only truthy value for penthouse
-                : item.beds !== undefined && item.beds !== null && item.beds !== '' // exclude empty bhk values
-        )
-        .map(item => [
-            item.key,
-            {
-                superArea: item.superArea,
-                builtupArea: item.builtupArea,
-                carpetArea: item.carpetArea,
-                img: item.img || ''
-            }
-        ])
-);
+    const configurationsData = Object.fromEntries(
+        bhkTypes
+            .filter(item =>
+                item.key === "Penthouse"
+                    ? !!item.beds // only truthy value for penthouse
+                    : item.beds !== undefined && item.beds !== null && item.beds !== '' // exclude empty bhk values
+            )
+            .map(item => [
+                item.key,
+                {
+                    superArea: item.superArea,
+                    builtupArea: item.builtupArea,
+                    carpetArea: item.carpetArea,
+                    img: item.img || ''
+                }
+            ])
+    );
 
     const [isSticky, setIsSticky] = useState(false);
     const [activeTab, setActiveTab] = useState('Unit Plan');
@@ -229,7 +229,7 @@ const configurationsData = Object.fromEntries(
                             </div>
                         )}
                     </div>
-                    
+
                     <div id="locality" className='locality-part'>
                         <h2>Locality</h2>
                         <LocalitySection project={project} />
@@ -237,7 +237,7 @@ const configurationsData = Object.fromEntries(
 
                     <div id="amenities" className='amenities_part'>
                         <h2>Amenities</h2>
-                        <AmenitiesSection style={{width:"100%"}} project={project}/>
+                        <AmenitiesSection style={{ width: "100%" }} project={project} />
                     </div>
 
                     <div id="bank" className='bank-part'>
@@ -247,12 +247,12 @@ const configurationsData = Object.fromEntries(
 
                     <div id="developer" className='bank-part'>
                         <h2>Developer</h2>
-                        <DeveloperSection project={project}/>
+                        <DeveloperSection project={project} />
                     </div>
 
                     <div id="projects" className='bank-part'>
                         <h2>Nearby Projects</h2>
-                        <NearByProject project={project}/>
+                        <NearByProject project={project} />
 
                     </div>
                 </div>
@@ -266,13 +266,13 @@ const configurationsData = Object.fromEntries(
                             <div className='contact-company-name-add'>
                                 <h5>{project?.Project_Name
                                     ? project.Project_Name.split(" ").map((word, index) => (
-                                        <span key={index} style={{ marginRight: "4px", fontSize:'15px' }}>
-                                            <span style={{ fontSize:'20px'}} className="large-letter">{word.charAt(0)}</span>
+                                        <span key={index} style={{ marginRight: "4px", fontSize: '15px' }}>
+                                            <span style={{ fontSize: '20px' }} className="large-letter">{word.charAt(0)}</span>
                                             {word.slice(1)}
                                         </span>
                                     ))
                                     : "N/A"}</h5>
-                                <p style={{color:'#2763ff'}}>{`${project.Localities}, ${project.Location}, ${project.City}`}</p>
+                                <p style={{ color: '#2763ff' }}>{`${project.Localities}, ${project.Location}, ${project.City}`}</p>
                             </div>
                         </div>
 
